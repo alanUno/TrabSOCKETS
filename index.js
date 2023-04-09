5fcc300fce34e749134f2c0a984cc724b7d429d5
ConcattoMatheus <concatto11matheus@gmail.com>
2023-04-09 20:37:51 -0300

    Finalizado

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const io = new Server(server);
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-// Conectar ao banco de dados
+// Conexão com o banco de dados
 const db = 'mongodb://localhost:27017/local';
+
 mongoose.connect(db, (err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  } else {
-    console.log('Conectado ao banco de dados');
-  }
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+    } else {
+        console.log('Conectado ao banco de dados');
+    }
 });
 
 // Definir o modelo de mensagens
@@ -22,7 +23,7 @@ const Mensagem = mongoose.model('Mensagem', {
     nome: String,
     mensagem: String,
     canal: String
-  });
+});
 
 // Configurar o servidor
 app.use(express.static(__dirname));
@@ -31,59 +32,54 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Obter todas as mensagens do banco de dados
 app.get('/mensagens', (req, res) => {
-  Mensagem.find({}, (err, mensagens) => {
-    if (err) {
-      console.error('Erro ao obter mensagens:', err);
-      res.sendStatus(500);
-    } else {
-      res.send(mensagens);
-    }
-  });
-});
-
-// Enviar mensagem para todos os clientes conectados
-io.on('connection', (socket) => {
-  console.log('Usuário conectado');
-
-  // Adicionar o socket à room correspondente ao canal selecionado pelo usuário
-  socket.on('join', (canal) => {
-    socket.join(canal);
-    console.log(`Usuário entrou no canal ${canal}`);
-  });
-
-  // Enviar mensagem para os clientes na mesma room (canal)
-socket.on('mensagem', (msg) => {
-  console.log(`Mensagem recebida no canal ${msg.canal}: ${msg.mensagem}`);
-  io.to(msg.canal).emit('mensagem', msg);
-  const mensagem = new Mensagem({
-    nome: msg.nome,
-    mensagem: msg.mensagem,
-    canal: msg.canal
-  });
-  mensagem.save((err) => {
-    if (err) {
-      console.error('Erro ao salvar mensagem:', err);
-    } else {
-      console.log('Mensagem salva no banco de dados');
-    }
-  });
-});
-
-  // Remover o socket da room correspondente ao canal ao desconectar
-  socket.on('disconnect', () => {
-    console.log('Usuário desconectado');
-    const rooms = Object.keys(socket.rooms);
-    rooms.forEach((room) => {
-      if (room !== socket.id) {
-        socket.leave(room);
-        console.log(`Usuário saiu do canal ${room}`);
-      }
+    Mensagem.find({}, (err, mensagens) => {
+        if (err) {
+            console.error('Erro ao obter mensagens:', err);
+            res.sendStatus(500);
+        } else {
+            res.send(mensagens);
+        }
     });
-  });
 });
 
-// Iniciar o servidor
+io.on('connection', (socket) => {
+    console.log('Usuário conectado');
+
+    socket.on('join', (canal) => {
+        socket.join(canal);
+        console.log(`Usuário entrou no canal ${canal}`);
+    });
+
+    socket.on('mensagem', (msg) => {
+        console.log(`Mensagem enviada no canal ${msg.canal}: ${msg.mensagem}`);
+        io.to(msg.canal).emit('mensagem', msg);
+        const mensagem = new Mensagem({
+            nome: msg.nome,
+            mensagem: msg.mensagem,
+            canal: msg.canal
+        });
+        mensagem.save((err) => {
+            if (err) {
+                console.error('Erro ao salvar mensagem:', err);
+            } else {
+                console.log('Mensagem salva com sucesso');
+            }
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+        const rooms = Object.keys(socket.rooms);
+        rooms.forEach((room) => {
+            if (room !== socket.id) {
+                socket.leave(room);
+                console.log(`Usuário saiu do canal ${room}`);
+            }
+        });
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Listen on ${PORT}`);
 });
